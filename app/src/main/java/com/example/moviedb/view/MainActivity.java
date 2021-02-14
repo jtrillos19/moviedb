7d43c63a8fd9abdb8e180a63843781459653c09c
Jesus Trillos <jesustrillos@wposs.com>
2021-02-14 13:35:13 -0500

    Mostrar Listado de las películas
    - Extendemos de BaseActivity para controlar el ciclo de vida de la activity cuando sea necesario y el presenter pueda saber el cambio de estado.
    - Implementamos el Presenter para obtener el listado de las peliculas con sus respectivos metodos que controlan los diferentes escenarios a la hora de procesar el listado
    - Ajustamos colores y tema para nuestra vista
    - Ajustamos el gitignore para que no me tenga en cuenta archivos de SonarLint

--- app/src/main/java/com/example/moviedb/view/MainActivity.java
+++ app/src/main/java/com/example/moviedb/view/MainActivity.java
@@ -1,16 +1,62 @@
 package com.example.moviedb.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.moviedb.Interface.ListaMoviesView;
 import com.example.moviedb.R;
+import com.example.moviedb.base.BaseActivity;
+import com.example.moviedb.model.ListaMoviesInteractor;
+import com.example.moviedb.model.Results;
+import com.example.moviedb.model.adapter.MoviesAdapter;
+import com.example.moviedb.presenter.ListaMoviesPresenter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends BaseActivity<ListaMoviesPresenter> implements ListaMoviesView {
+
+    private MoviesAdapter moviesAdapter;
+    private RecyclerView rvMovies;
+
+
+    @NonNull
+    @Override
+    protected ListaMoviesPresenter createPresenter(@NonNull Context context) {
+        return new ListaMoviesPresenter(this,new ListaMoviesInteractor());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rvMovies = findViewById(R.id.rvMovies);
+        getMovies();
+    }
+
+    @Override
+    public void getMovies() {
+        mPresenter.cargarDatos();
+    }
+
+    @Override
+    public void cargarRecylcer(ArrayList<Results> results) {
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        rvMovies.setLayoutManager(layoutManager);
+        moviesAdapter = new MoviesAdapter(results,this);
+        rvMovies.setAdapter(moviesAdapter);
+    }
+
+    @Override
+    public void mostrarError() {
+        Toast.makeText(this, "Error intentando cargar los datos ⛔", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void noHayResultados() {
+        Toast.makeText(this, "No hay datos ⛔", Toast.LENGTH_SHORT).show();
     }
 }
